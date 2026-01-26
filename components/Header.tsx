80ad461bac5fa08a8681c5dde64ac52e9075f041
rose-ramasamy <rose.ramasamy@philips.com>
2026-01-26 09:01:10 +0530

    Fix logo path to include basePath for GitHub Pages

--- components/Header.tsx
+++ components/Header.tsx
@@ -39,7 +39,7 @@ export default function Header() {
           <div className="flex items-center">
             <Link href="/" className="flex items-center">
               <img
-                src="/simple-bb-logo.svg"
+                src="/beyondbooking/simple-bb-logo.svg"
                 alt="Beyond Booking"
                 className="h-10 w-auto"
               />
